f965e7293778541aac7fb33dd845d5803d8281bc
Swagatam Pati <swagatamheroz@outlook.com>
2024-10-29 13:16:56 +0530

    feat: implement trim function to format source code

--- asm.c
+++ asm.c
@@ -1,7 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "./utils.c"
+
+typedef struct {
+    char** data;
+    int size;
+    int capacity;
+} Vector;
+
+void initVector(Vector *vec) {
+    vec->size = 0;
+    vec->capacity = 4;
+    vec->data = malloc(sizeof(char*) *vec->capacity);
+}
+void addElement(Vector* vec, const char* str) {
+    if (vec->size >= vec->capacity) {
+        vec->capacity *= 2;
+        vec->data = realloc(vec->data, sizeof(char*) *vec->capacity);
+    }
+    vec->data[vec->size] = strdup(str);
+    vec->size++;
+}
+void freeVector(Vector* vec) {
+    int i;
+    for (i = 0; i < vec->size; i++) {
+        free(vec->data[i]);
+    }
+    free(vec->data);
+}
+
+const char* getElement(Vector* vec, int index) {
+    if (index >= 0 && index < vec->size) {
+        return vec->data[index];
+    }
+    return NULL;
+}
+
+/***************************************
+    formats source code line-by-line    
+***************************************/
+char* trim(char* line, int size) {
+    char* trimmedStr = (char*) malloc(size);
+
+    int spaces = 0;
+
+    int i;
+
+    for (i = 0; i < 2; i ++) {
+        strrev(line);
+        while (size > 0 && (line[size - 1] == ' ' || line[size - 1] == '\t')) {
+            line[size - 1] = '\0';
+            size --;
+        }
+    }
+
+    trimmedStr[0] = '\0';
+    for (i = 0; i < size; i ++) {
+        int j = i;
+
+        if (line[i] == ';') {
+            break;
+        }
+
+        if (line[i] == ':') {
+            strcat(trimmedStr, ":");
+            if (i == size - 1 || line[i + 1] != ' ') {
+                strcat(trimmedStr, " ");
+            }
+            continue;
+        }
+
+        if (line[i] != ' ' && line[i] != '\t') {
+            strncat(trimmedStr, &line[i], 1);
+            continue;
+        }
+
+        strcat(trimmedStr, " ");
+
+        while (line[i] == line[j] && j < size) {
+            j ++;
+        }
+        i = j - 1;
+    }
+
+    while (strlen(trimmedStr) > 0 && (trimmedStr[strlen(trimmedStr) - 1] == ' ' || trimmedStr[strlen(trimmedStr) - 1] == '\t')) {
+        trimmedStr[strlen(trimmedStr) - 1] = '\0';
+    }
+    
+    for (i = 0; (unsigned) i < strlen(trimmedStr); i ++) {
+        if (trimmedStr[i] == ' ') {
+            spaces ++;
+        }
+    }
+
+    if (spaces > 2) {
+        printf("Invalid Syntax.\n");
+    }
+    return trimmedStr;
+}
+
+Vector sourceCode;
 
 /****************************************************
     executes the first pass of the assembly cycle    
@@ -18,21 +118,34 @@ void executePass1(char* sourceFilePath) {
         printf("Sucessfully loaded \"%s\"\n", sourceFilePath);
     }
 
+    initVector(&sourceCode);
+    
     while ((line = readLine(sourceFile)) != NULL) {
-        printf("%s\n", line);
-        free(line);
+        char* tmp = trim(line, strlen(line));
+        if (strlen(tmp) > 0) {
+            addElement(&sourceCode, tmp);
+        }
     }
 
     fclose(sourceFile);
 }
 
 int main(int argc, char* argv[]) {
+    int i;
+
     if (argc != 2) {
         fprintf(stderr, "ASSEMBLER_ERROR: Usage: ./a <source-file-path>\n");
         exit(-1);
     }
 
     executePass1(argv[1]);
+
+    printf("%d\n", sourceCode.size);
+    for (i = 0; i < sourceCode.size; i ++) {
+        printf("%s\n", getElement(&sourceCode, i));
+    }
+
+    freeVector(&sourceCode);
 
     return 0;
 }
--- utils.c
+++ utils.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**********************************************
     reads a line from a file and returns it    
@@ -35,4 +36,38 @@ char* readLine(FILE* filePtr) {
 
     buffer[len] = '\0';
     return buffer;
+}
+
+
+/*****************************************
+    duplicates a string and returns it    
+*****************************************/
+char *strdup(const char *str) {
+    size_t size = strlen(str) + 1;
+    char* p = malloc(size);
+    if (p) {
+        memcpy(p, str, size);
+    }
+    return p;
+}
+
+/*********************************
+    reverses a string in-place    
+*********************************/
+void strrev(char* str) {
+    int i = 0;
+    int j = strlen(str) - 1;
+
+    if (!str) {
+        return;
+    }
+    
+    while (i < j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+
+        i ++;
+        j --;
+    }
 }
